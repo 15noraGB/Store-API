0d536fc16c58d5de8297cc1f6fb4f38040298a18
noratiangb <noratiangb@gmail.com>
2025-12-04 11:23:02 +0000

    Store API

--- index.ts
+++ index.ts
@@ -1,20 +1,17 @@
 import * as express from "express";
 import * as path from "path";
+import productRoutes from "./endpoints/products";
 
 const app = express();
 const port = parseInt(process.env.PORT) || process.argv[3] || 8080;
 
-app.use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs');
+
 
 app.get('/', (req, res) => {
-  res.render('index');
+  res.send('querida!');
 });
 
-app.get('/api', (req, res) => {
-  res.json({"msg": "Hello world"});
-});
+
 
 app.listen(port, () => {
   console.log(`Listening on http://localhost:${port}`);
---
+++ interfaces/product.ts
@@ -0,0 +1,17 @@
+export interface Product {
+    id: number;
+    title: string;
+    price: number;
+    description: string;
+    category: string;
+    image: string;
+    rating: {
+        rate: number;
+        count: number;
+    };
+}
+
+
+export type CreateProductDTO = Omit<Product, 'id'>;
+export type UpdateProductDTO = Partial<Product>;
+
